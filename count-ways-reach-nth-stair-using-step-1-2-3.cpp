1d0d4c519ac75706dc189bee53d2286888c875b4
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2020-05-03 14:33:55 +0530

    Add files via upload

---
+++ count-ways-reach-nth-stair-using-step-1-2-3.cpp
@@ -0,0 +1,30 @@
+//https://www.geeksforgeeks.org/count-ways-reach-nth-stair-using-step-1-2-3/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t,n;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		int dp[n+1]={0};
+		dp[0]=1;
+		dp[1]=1;
+		
+		for(int i=2;i<n+1;i++){
+			if(i>=1)
+			dp[i]+=dp[i-1];
+			if(i>=2)
+			dp[i]+=dp[i-2];
+			if(i>=3)
+			dp[i]+=dp[i-3];
+			
+		}
+		
+		//for(int i=0;i<n+1;i++){
+		//	cout<<i<<"      "<<dp[i]<<endl;
+		//}
+		//cout<<endl;
+		cout<<dp[n]<<endl;
+	}	
+}
